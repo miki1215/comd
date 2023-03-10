c77e8690ede9ce08dc22bbeceb4e1c364a4d0f8f
miki1215 <66144549+miki1215@users.noreply.github.com>
2023-03-10 02:24:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo rm microsoft.gpg
 sudo apt update && sudo apt install -y microsoft-edge-stable
+
+sudo apt install -y freerdp2-dev
+
